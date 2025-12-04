19363529de6c1e95898a1f3621cf6fa73aa33549
Mike Miller <millms302@gmail.com>
2025-12-04 17:26:15 -0600

    assignment3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 function Home() {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Home Page.</h1>
             <p>Welcome to My Online Store</p>
 
             <img src="https://images.unsplash.com/photo-1751759196158-a1eb714a2dec?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
