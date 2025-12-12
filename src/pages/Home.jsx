e3949f34e58cc0f7a496eda11b56fe497c8e1d44
Mike Miller <millms302@gmail.com>
2025-12-12 14:18:10 -0600

    presentation_final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ function Home() {
         <div className="d-flex align-center">
             <h1>Selling Sticks to Shredders Since 2025</h1>
             
-            <img src="https://images.unsplash.com/photo-1751759196158-a1eb714a2dec?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-            <h1>Home of the 30 Try and ReTry Return Program.</h1>
+            <img src="./images/OpenSign.png"/>
+            <h1>Home of the 30-Day Try and Return Program.</h1>
         </div>
     )
 }
